b112b714ebfd024f8d77706dbe43882d7497dde1
Roman <roman.gajardo@gmail.com>
2020-10-14 23:09:59 -0300

    mode dark y detail terminado en un 90%

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import countries from "./modules/countries.js";
 window.printAllCountries = countries.printAllCountries.bind(countries);
 window.filterByRegion = countries.filterByRegion.bind(countries);
 window.filterByText = countries.filterByText.bind(countries);
+window.country_event = countries.country_event.bind(countries);
 
 var countries_panel = document.getElementById("countries");
 
@@ -12,7 +13,6 @@ const api = async () => {
     const data = await res.json();
     countries_panel.innerHTML = countries.printAllCountries(data);
     document.getElementById("region").addEventListener("change", (e) => {
-      console.log(e.target.value);
       if (e.target.value != "") {
         countries_panel.innerHTML = countries.filterByRegion(
           data,
@@ -25,39 +25,80 @@ const api = async () => {
     document.getElementById("txt").addEventListener("keyup", (e) => {
       countries_panel.innerHTML = countries.filterByText(data, e.target.value);
     });
+    document.getElementById("btn-back").addEventListener("click", (e) => {
+      document.getElementById("btn-back").style.display = "none";
+      document.getElementById("txt").style.display = "block";
+      document.getElementById("region").style.display = "block";
+      countries_panel.innerHTML = countries.printAllCountries(data);
+      if (countries.light) {
+        modeLight();
+      } else {
+        modeDark();
+      }
+    });
   } catch (error) {
     console.log(error);
   }
 };
 api();
 
-var light = true;
+//var light = true;
 
 document.getElementById("mode").addEventListener("click", () => {
-  if (light) {
-    document.querySelector("nav").classList.add("dark-nav");
-    document.querySelector("body").classList.add("dark-body");
-    document.querySelector("input").classList.add("dark-controls");
-    document.querySelector("select").classList.add("dark-controls");
-    var cards = document.getElementsByClassName("card");
+  if (countries.light) {
+    modeDark();
+    countries.light = false;
+  } else {
+    modeLight();
+    countries.light = true;
+  }
+});
 
-    for (var i = 0; i < 250; i++) {
+function modeDark() {
+  document.querySelector("nav").classList.add("dark-nav");
+  document.querySelector("body").classList.add("dark-body");
+  document.querySelector("input").classList.add("dark-controls");
+  document.querySelector("select").classList.add("dark-controls");
+  var btn = document.getElementById("btn-back");
+
+  btn.style.background = "var(--DarkBlueElements)";
+  btn.style.color = "white";
+  btn.style.boxShadow = "0px 0px 1px black";
+  var cards = document.getElementsByClassName("card");
+  if (cards.length != 0) {
+    for (var i = 0; i < cards.length; i++) {
       cards[i].style.background = "var(--DarkBlueElements)";
       cards[i].style.color = "white";
       cards[i].style.boxShadow = "0px 0px 1px black";
     }
-    light = false;
-  } else {
-    document.querySelector("nav").classList.remove("dark-nav");
-    document.querySelector("body").classList.remove("dark-body");
-    document.querySelector("select").classList.remove("dark-controls");
-    document.querySelector("input").classList.remove("dark-controls");
-    var cards = document.getElementsByClassName("card");
+  }
+  var textDetail = document.getElementById("text-detail");
+  if (textDetail != null) {
+    textDetail.style.color = "white";
+  }
+}
+
+function modeLight() {
+  document.querySelector("nav").classList.remove("dark-nav");
+  document.querySelector("body").classList.remove("dark-body");
+  document.querySelector("select").classList.remove("dark-controls");
+  document.querySelector("input").classList.remove("dark-controls");
+  var btn = document.getElementById("btn-back");
+
+  btn.style.background = "white";
+  btn.style.color = "black";
+  btn.style.boxShadow = "0px 0px 1px rgb(177, 174, 174)";
+
+  var cards = document.getElementsByClassName("card");
+  if (cards.length != 0) {
     for (var i = 0; i < 250; i++) {
       cards[i].style.background = "white";
       cards[i].style.color = "black";
       cards[i].style.boxShadow = "0px 0px 1px rgb(177, 174, 174)";
     }
-    light = true;
   }
-});
+  var textDetail = document.getElementById("text-detail");
+  if (textDetail != null) {
+    textDetail.style.color = "black";
+  }
+}
--- js/modules/countries.js
+++ js/modules/countries.js
@@ -1,7 +1,7 @@
 export default {
+  light: true,
   printAllCountries: function (data) {
     var card = "";
-    console.log(data);
     data.forEach((item) => {
       card += this.card_country(item);
     });
@@ -28,7 +28,7 @@ export default {
   },
   card_country: function (country) {
     return `
-      <div class="card">
+      <div onclick="country_event('${country.name}')" class="card">
         <div class="img-container">
         <img  src="${country.flag}" alt="" />
         </div>
@@ -40,5 +40,58 @@ export default {
         </p>
       </div>    
     `;
+  },
+  country_event: async function (name) {
+    document.getElementById("btn-back").style.display = "block";
+
+    document.getElementById("txt").style.display = "none";
+    document.getElementById("region").style.display = "none";
+    const res = await fetch("https://restcountries.eu/rest/v2/all");
+    const data = await res.json();
+    var country_array = data.filter((country) => country.name === name);
+    this.paint_detail(country_array[0]);
+    console.log(country_array[0]);
+  },
+
+  paint_detail(country) {
+    var panel = document.getElementById("countries");
+    panel.innerHTML = `
+      
+        <div class="content-detail">
+          <div class="img-detail">
+            <img  src="${country.flag}" alt="" />
+          </div>
+          <div id="text-detail" class="text-detail">
+            <div class="text-detail-left">
+              <h3>${country.name}</h3>
+              <p style="padding:0px">Native Name: ${country.nativeName}</p>
+              <p style="padding:0px">Population: ${country.population}</p>
+              <p style="padding:0px">Region: ${country.region}</p>
+              <p style="padding:0px">Sub Region: ${country.subregion}</p>
+              <p style="padding:0px">Capital: ${country.capital}</p>
+            </div>
+            <div class="text-detail-right">
+              <h3>&nbsp;</h3>
+              <p style="padding:0px">Top Level Domain: ${
+                country.topLevelDomain
+              }</p>
+              <p style="padding:0px">Currencies: ${country.currencies.map(
+                (xx) => xx.name
+              )}</p>
+              <p style="padding:0px">Languajes: ${country.languages.map(
+                (xx, index) => xx.name
+              )}</p>
+              
+            </div>
+
+          </div>
+        </div>
+      
+    `;
+    if (this.light) {
+      document.getElementById("text-detail").style.color = "black";
+    } else {
+      document.getElementById("text-detail").style.color = "white";
+    }
   },
 };
